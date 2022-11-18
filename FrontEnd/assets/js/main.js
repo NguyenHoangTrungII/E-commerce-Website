15f343b9995f97b8128924266985ff0f224edec0
nhatsinh2002 <nhatsinh2002.pt@gmail.com>
2022-11-18 17:07:33 +0700

    commit

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -43,7 +43,31 @@
         allowParentLinks: true
     });
 
-    /*------------------
+
+})(jQuery)
+
+// nút tăng giảm số lượng
+$('input.input-qty').each(function() {
+    var $this = $(this),
+      qty = $this.parent().find('.is-form'),
+      min = Number($this.attr('min')),
+      max = Number($this.attr('max'))
+    if (min == 0) {
+      var d = 0
+    } else d = min
+    $(qty).on('click', function() {
+      if ($(this).hasClass('minus')) {
+        if (d > min) d += -1
+      } else if ($(this).hasClass('plus')) {
+        var x = Number($this.val()) + 1
+        if (x <= max) d += 1
+      }
+      $this.attr('value', d).val(d)
+    })
+  })
+
+
+      /*------------------
        Product List
     --------------------*/
     // For Filters
@@ -172,24 +196,3 @@
             thumbRight.classList.remove("active");
         });
     });
-})(jQuery)
-
-// nút tăng giảm số lượng
-$('input.input-qty').each(function() {
-    var $this = $(this),
-      qty = $this.parent().find('.is-form'),
-      min = Number($this.attr('min')),
-      max = Number($this.attr('max'))
-    if (min == 0) {
-      var d = 0
-    } else d = min
-    $(qty).on('click', function() {
-      if ($(this).hasClass('minus')) {
-        if (d > min) d += -1
-      } else if ($(this).hasClass('plus')) {
-        var x = Number($this.val()) + 1
-        if (x <= max) d += 1
-      }
-      $this.attr('value', d).val(d)
-    })
-  })
