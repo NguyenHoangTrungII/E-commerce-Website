92222abe1f5b0c89c4a3ab5707e1a1932dcab8f9
nhatsinh2002 <nhatsinh2002.pt@gmail.com>
2022-12-13 08:28:55 +0700

    commmit

---
+++ DoAnWEb_TestMVC/Admin/View/list_customer.php
@@ -0,0 +1,251 @@
+<?php
+  include("include/top.php");
+  include("include/menu.php");
+?>
+
+        <!-- Content wrapper -->
+        <div class="content-wrapper">
+          <!-- Content -->
+
+          <div class="container-xxl flex-grow-1 container-p-y">
+            <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Nhân lực /</span> Khách hàng</h4>
+
+            <hr class="my-5" />
+
+            <!-- Bootstrap Table with Header - Footer -->
+            <div class="card">
+              <div class="card-header header table-user">
+                <h5 class=" card-header">Thông tin khách hàng</h5>
+                <a href="add_customer.php"><button class="btn btn-primary add-btn user" type="button">THÊM</button></a>
+              </div>
+              <div class="table-responsive text-nowrap">
+                <table class="table user">
+                  <thead>
+                    <tr>
+                      <th class="id-header user">Mã</th>
+                      <th class="name user">Họ tên</th>
+                      <th class="img user">Ảnh</th>
+                      <th class="birthday user">Ngày sinh</th>
+                      <th class="email user">Email</th>
+                      <th class="phone user">Số điện thoại</th>
+                      <th class="address user">Địa chỉ</th>
+                      <th class="action user">Thao tác</th>
+
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="id-header user">
+                        1
+                      </td>
+                      <td class="name user">
+                        Nguyễn Hoàng Trung
+                      </td>
+                      <td class="img user">
+                        <img src="../../public/uploads/users/User_admin_anh-dai-dien.jpg" alt="Anh dai dien" class="photo-user">
+                      </td>
+                      <td class="birthday user">
+                        1/1/2022
+                      </td>
+                      <td  class="email user">
+                        nguyenhoangtrung@gmail.com
+                      </td>
+                      <td class="phone user">
+                        0123456789
+                      </td>
+                      <td class="address user">
+                        Dắk lắk, Buôn Ma Thuột, xã Hòa Phú, đường nào đaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                      </td>
+                      <td class="action user">
+                        <div class="dropdown">
+                          <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                            <i class="bx bx-dots-vertical-rounded"></i>
+                          </button>
+                          <div class="dropdown-menu action--none">
+                            <a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modalLong"><i
+                                class="bx bx-info-circle"></i> Xem thông tin chi tiết</a>
+
+                            <a class="dropdown-item" href="edit_customer.php"><i class="bx bx-edit-alt me-1"></i>
+                              Sửa</a>
+                            <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Xóa</a>
+                          </div>
+
+
+                        </div>
+
+                      </td>
+
+                    </tr>
+                  </tbody>
+
+                  <tfoot class="table-border-bottom-0">
+                    <tr>
+                      <th class="id-header user">Mã</th>
+                      <th class="name user">Họ tên</th>
+                      <th class="img user">Ảnh</th>
+                      <th class="birthday user">Ngày sinh</th>
+                      <th class="email user">Email</th>
+                      <th class="phone user">Số điện thoại</th>
+                      <th class="address user">Địa chỉ</th>
+                      <th class="action user">Thao tác</th>
+                    </tr>
+                  </tfoot>
+
+
+
+
+                  <div class="modal fade" id="modalLong" tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="modalLongTitle">Thông tin chi tiết</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          <div class="row">
+                            <div class="col-xl">
+                              <div class="card mb-4">
+                                <div class="card-header d-flex justify-content-between align-items-center">
+                                  <h5 class="mb-0">Thông tin <span>nhân viên</span></h5>
+                                  <small class="text-muted float-end">Thêm thông tin</small>
+                                </div>
+                                <div class="card-body">
+                                  <form>
+                                    <div class="row">
+                                      <div class="mb-3 col-xl-6">
+                                        <label class="form-label" for="basic-default-fullname">Họ
+                                          tên</label>
+                                        <input type="text" class="form-control" id="basic-default-fullname"
+                                          placeholder="Nhập họ và tên" disabled />
+                                      </div>
+                                      <div class="mb-3 col-xl-6">
+                                        <label class="form-label">Ngày sinh</label>
+                                        <input class="form-control" type="date" value="Chọn ngày sinh" disabled>
+                                      </div>
+                                    </div>
+                                    <div class="row">
+                                      <div class="form-check col-xl-4">
+                                        <label class="form-label mb-3" for="basic-default-phone">Giới
+                                          tính</label>
+                                        <div class="row px-4">
+                                          <div class="col-6">
+                                            <input name=" GioiTinh" class="form-check-input" type="radio" value=""
+                                              checked>
+                                            <label class="form-check-label" for="defaultRadio2"> Nam
+                                            </label>
+                                          </div>
+
+                                          <div class="col-6">
+                                            <input name="GioiTinh" class="form-check-input" type="radio" value="">
+                                            <label class="form-check-label" for="defaultRadio1">
+                                              Nữ </label>
+                                          </div>
+                                        </div>
+                                      </div>
+
+
+                                      <div class="col-xl-4">
+                                        <div class="mb-3">
+                                          <label class="form-label" for="basic-default-email">Email</label>
+                                          <div class="input-group input-group-merge">
+                                            <input type="text" id="basic-default-email" class="form-control"
+                                              placeholder="nguyenhoangtrunghs" aria-label=""
+                                              aria-describedby="basic-default-email2" disabled />
+                                            <!-- <span class="input-group-text" id="basic-default-email2">@gmail.com</span> -->
+                                          </div>
+                                          <!-- <div class="form-text">You can use letters, numbers & periods</div> -->
+                                        </div>
+                                      </div>
+                                      <div class="col-xl-4">
+                                        <div class="mb-3">
+                                          <label class="form-label" for="basic-default-phone">Số điện
+                                            thoại</label>
+                                          <input type="text" id="basic-default-phone" class="form-control phone-mask"
+                                            placeholder="0123456789" disabled />
+                                        </div>
+                                      </div>
+
+
+                                    </div>
+
+                                    <div class="row">
+                                      <div class="col-xl-4">
+                                        <div class="mb-3">
+                                          <label for="defaultSelect" class="form-label">Tỉnh/Thành
+                                            phố</label>
+                                          <select id="defaultSelect" class="form-select" disabled>
+                                            <option>Chọn tỉnh/thành phố</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                          </select>
+                                        </div>
+                                      </div>
+
+                                      <div class="col-xl-4">
+                                        <div class="mb-3">
+                                          <label for="defaultSelect" class="form-label">Quận/Huyện</label>
+                                          <select id="defaultSelect" class="form-select" disabled>
+                                            <option>Chọn quận/huyên</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                          </select>
+                                        </div>
+                                      </div>
+
+
+                                      <div class="col-xl-4">
+                                        <div class="mb-3">
+                                          <label for="defaultSelect" class="form-label">Phường/Xã</label>
+                                          <select id="defaultSelect" class="form-select" disabled>
+                                            <option>Chọn phường/xã</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                          </select>
+                                        </div>
+                                      </div>
+                                    </div>
+
+                                    <div class="mb-3">
+                                      <label class="form-label" for="basic-default-message">Địa chỉ</label>
+                                      <textarea id="basic-default-message" class="form-control" placeholder="Địa chỉ"
+                                        disabled></textarea>
+                                    </div>
+
+                                    <div class="mb-5">
+                                      <label for="formFile" class="form-label">Ảnh</label>
+                                      <div class="fileupload fileupload-new border-5" data-provides="fileupload">
+                                        <div class="fileupload-new thumbnail" style="width: 160px; height: 160px;">
+                                          <img src="../assets/img/avatars/1.png" alt="">
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                            Đóng
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+                </table>
+              </div>
+            </div>
+            <!-- Bootstrap Table with Header - Footer -->
+          </div>
+          <!-- / Content -->
+        
+        
+        <?php
+          include("include/tail.php");
+        ?>
